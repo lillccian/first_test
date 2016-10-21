3b327d0a55928ea74b53d2b9b07a21906e9a51f5
lillccian <linciansian@linde-MacBook-Air.local>
2016-10-21 10:24:25 +0800

    first commit

---
+++ test.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def rob(nums)
+    p , n = 0 , 0
+    nums.each do |nw|
+        p , n =
+        n + nw , [p , n].max
+    end
+    return [p , n].max
+end
+a = [127,188,240,241,164,137,123,78,118,84,210,242,87,214,237,206,183,210,81,171,66,80,109,65,76,109]
+puts rob(a)
